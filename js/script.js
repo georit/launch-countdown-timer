8626f9a84eeadfc5e603580b16a157526bab7e61
Itai Mapaya <mambostudioz@gmail.com>
2021-01-07 17:27:06 -0600

    Minor edits

--- js/script.js
+++ js/script.js
@@ -9,12 +9,20 @@ const countDownTimer = setInterval(countDownFn, 1000);
 function countDownFn() {
   const now = new Date().getTime();
   const timeLeft = countDownDate - now;
-  const days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
-  const hours = Math.floor(
-    (timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  const days = String(Math.floor(timeLeft / (1000 * 60 * 60 * 24))).padStart(
+    2,
+    "0"
   );
-  const minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
-  const seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
+  const hours = String(
+    Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+  ).padStart(2, "0");
+  const minutes = String(
+    Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60))
+  ).padStart(2, "0");
+  const seconds = String(Math.floor((timeLeft % (1000 * 60)) / 1000)).padStart(
+    2,
+    "0"
+  );
 
   // update html
   mainHeading.innerHTML = `WE'RE LAUNCHING SOON`;
